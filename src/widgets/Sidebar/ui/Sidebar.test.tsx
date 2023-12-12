5b01960d382dbedd587e8cf42a06a9991f12e35e
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2023-12-12 14:18:57 +0200

    fix screens, add visual tool

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,18 +1,19 @@
 import { screen, fireEvent } from '@testing-library/react'
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender'
 import { renderWithTranslation } from 'shared/lib/tests/renderWithTranslation/renderWithTranslation'
 import { Sidebar } from './Sidebar'
 
 describe('Sidebar', () => {
     test('Test', () => {
-        renderWithTranslation(<Sidebar />)
+        componentRender(<Sidebar />)
         expect(screen.getByTestId('sidebar')).toBeInTheDocument()
     })
 
     test('Test toggle', () => {
-        renderWithTranslation(<Sidebar />)
-		  const toggleBtn = screen.getByTestId('sidebar-toggle')
+        componentRender(<Sidebar />)
+        const toggleBtn = screen.getByTestId('sidebar-toggle')
         expect(screen.getByTestId('sidebar')).toBeInTheDocument()
-		  fireEvent.click(toggleBtn)
-		  expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
+        fireEvent.click(toggleBtn)
+        expect(screen.getByTestId('sidebar')).toHaveClass('collapsed')
     })
 })
