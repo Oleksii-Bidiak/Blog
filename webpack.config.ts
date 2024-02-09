5732e40c5ad3f6ec1f3d77ded8fd7b0f1b52646b
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2024-02-09 12:57:05 +0200

    fix bugs

--- webpack.config.ts
+++ webpack.config.ts
@@ -10,13 +10,12 @@ export default (env: BuildEnv) => {
         html: path.resolve(__dirname, 'public', 'index.html'),
         src: path.resolve(__dirname, 'src'),
         locales: path.resolve(__dirname, 'public', 'locales'),
-        buildLocales: path.resolve(__dirname, 'public', 'locales'),
+        buildLocales: path.resolve(__dirname, 'build', 'locales'),
     }
 
     const mode = env.mode || 'development'
     const PORT = env.port || 3000
     const apiUrl = env.apiUrl || 'http://localhost:8000'
-    console.log(apiUrl)
 
     const isDev = mode === 'development'
 
